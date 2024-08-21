b8c762e3969d4c055476b392ca40d0b83070b689
ErickOlivarr <131342527+ErickOlivarr@users.noreply.github.com>
2024-08-20 19:32:42 -0600

    correccion health-check

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,8 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
-import { HealthCheckModule } from './health-check/health-check.module';
 
 @Module({
-  imports: [AuthModule, HealthCheckModule],
+  imports: [AuthModule],
   controllers: [],
   providers: [],
 })
--- src/health-check/health-check.controller.ts
+++
@@ -1,9 +0,0 @@
-import { Controller, Get } from '@nestjs/common';
-
-@Controller('/')
-export class HealthCheckController {
-    @Get()
-    healthCheck() {
-        return 'auth-ms is up running';
-    }
-}
